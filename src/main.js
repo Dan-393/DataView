1375362c5d8ce40888a3a71ef8f07fcd4cd686bd
Dan-393 <54181600+Dan-393@users.noreply.github.com>
2021-04-27 10:34:06 +0800

    基本配置^_^

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入全局样式
+import './assets/css/global.less'
+import './assets/lib/theme/chalk'
+// 引入全局echarts
+import echarts from 'echarts'
+import axios from 'axios'
+// 请求基准路径 都需要加上api 
+axios.defaults.baseURL = 'http://10.22.48.255:8080/api/'
+// 全局挂载
+Vue.prototype.$echarts = echarts
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
